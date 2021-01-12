e7fe8171b2c121f5ee62c4fb910a65f6a4a33d27
Ken Tauchi <kentauchica@gmail.com>
2021-01-11 23:42:08 -0800

    change content about me

--- src/data/MyData.js
+++ src/data/MyData.js
@@ -1,17 +1,17 @@
 export const MyData = [
   {
-    title: "What do I do?",
+    title: "Ken is ...",
     desc:
-      "I am a FrontEnd/FullStack developer, currently working for Langara College as a contract in Vancouver, Canada attending the college as a student as well.",
+      "a FrontEnd/FullStack developer, currently working for Langara College as a contract in Vancouver, Canada attending the college as a student as well.",
   },
   {
-    title: "What can I do for you?",
+    title: "Communication",
     desc:
       "I have strong communication skills gained from 4 years of experience in sales. Goal-oriented and works while flexible to changes. Maximize the use of resources to exchange ideas and information to achieve an improvement in efficiency and influence others.",
   },
   {
-    title: "Who am I like?",
+    title: "Motto",
     desc:
-      "I love to challenge myself. My motto is to be better than yesterday by learning new things every day. After I noticed myself enjoying learning new things, my English grade improved from the second worst in my high school to one of the tops. I eventually ended up getting a bachelor of buiness in the US and trying to become a professional developer in Canada now.",
+      "I love to challenge myself. My motto is to be better than yesterday by learning new things every day. After I noticed myself enjoying learning new things, my English grade improved from the second worst in my high school to one of the tops. I eventually ended up getting a bachelor of buiness in the US and now looking for an oppotunity to become a professional developer in Canada. I will never stop challenging.",
   },
 ];
