f7033ac687416ee4058f376e8321d1bc07354c00
Ken Tauchi <kentauchica@gmail.com>
2021-04-09 01:05:32 -0700

    add new work

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,27 +1,67 @@
+import pivotcarewebImg from "../assets/pivotcareweb.jpg";
+import pivotcaremobileImg from "../assets/pivotcaremobile.jpg";
 import nearestImg from "../assets/nearest.png";
 import naniwaismImg from "../assets/naniwaism.jpg";
 import savyImg from "../assets/savy.jpg";
 import langaraImg from "../assets/langara.jpg";
+import langaraImgV2 from "../assets/langarav2.jpeg";
 import portfolioImg from "../assets/portfolio.jpg";
 import quizapp from "../assets/quizapp.jpg";
 
 export const ProjectData = [
   {
+    category: "React Native Mobile App",
+    desc:
+      "The app helps companies continue providing fitness and mental health care for their employees. Web app for employer and instructors are also available.",
+    tech: ["React Native", "Expo", "Redux", "MySQL", "Node.js"],
+    link: "https://pivotcare.ca/",
+    img: pivotcaremobileImg,
+    bcgColor: "linear-gradient(90deg, #7662a5, #5C4D80)",
+    demoLink: null,
+  },
+  {
+    category: "React Web Admin App",
+    desc:
+      "The app helps companies continue providing fitness and mental health care for their employees. Mobile app for employee is also available.",
+    tech: [
+      "React",
+      "React Hooks",
+      "Redux",
+      "Styled-Component",
+      "MySQL",
+      "Node.js",
+    ],
+    link: "https://pivotcare.ca/",
+    img: pivotcarewebImg,
+    bcgColor: "linear-gradient(90deg, #7662a5, #5C4D80)",
+    demoLink: null,
+  },
+  {
     category: "React Data Driven Website",
     desc:
       "An educational data driven responsive platform that aims to help people who are concerned about our environment to get more information about recycling.",
-    tech: ["React", "React Hooks", "React Redux", "SASS", "MySQL", "Node.js"],
+    tech: ["React", "React Hooks", "Redux", "SASS", "MySQL", "Node.js"],
     link: "https://www.savy-app.ca/",
     img: savyImg,
     bcgColor: "linear-gradient(90deg, #229954, #1D8348)",
     demoLink: null,
   },
   {
-    category: "React Official College Website",
+    category: "React Official College Website V2",
     desc:
       "Langara Web and Mobile Development Program Official Website. The website is a headless CMS.",
+    tech: ["React", "Next.js", "Styled Component", "API", "WordPress"],
+    link: "https://langara-app.vercel.app",
+    img: langaraImgV2,
+    bcgColor: "linear-gradient(90deg, #c36448, #893701)",
+    demoLink: null,
+  },
+  {
+    category: "React Official College Website MVP",
+    desc:
+      "MVP of Langara Web and Mobile Development Program Official showcase Website. The website is a headless CMS.",
     tech: ["React", "React Hooks", "SASS", "API", "PHP", "WordPress"],
-    link: "https://langara-app.ca",
+    link: "https://zen-curran-2f42a7.netlify.app",
     img: langaraImg,
     bcgColor: "linear-gradient(90deg, #D35400, #A04000)",
     demoLink: null,
